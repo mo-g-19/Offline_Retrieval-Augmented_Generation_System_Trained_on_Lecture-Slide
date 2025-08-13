abcbd8a3a6e71fc7342e4bdae0c385321dd57f33
mo-g-19 <sggibson619@gmail.com>
2025-08-13 15:31:32 -0700

    Adding comments to readSlides.py

--- readSlides.py
+++ readSlides.py
@@ -1,27 +1,34 @@
 """The purpose of this document is to take the text from the pdf version of the slides and convert it into a dictionary where the key is the slide number and the value is the text"""
 
-import fitz #pyMuPDF library
-import json
+import fitz     #pyMuPDF library: to read text from a pdf (needed to install)
+import json     #to save the individual presentation as a document with a dictionary that has a value for the slides and text in the slides
 
+#The pdf document to read from and the new document to save information
 input_doc = "06_Naming.pdf"
 output_doc = "lecture06_processed.json"
 
 
 doc = fitz.open(input_doc)
 
+#The single array that holds the dictionaries
 slides = []
 
-for number, page in enumerate(doc):     #to go through in order of the slides
-    raw_text = page.get_text()
-    lines = [line.strip() for line in raw_text.split('\n') if line.strip()]     #creates a list of stripped line that doesn't have any empty lines
-    indv_slide_txt = " ".join(lines)    #puts all the lines from one slide into one paragraph
-    #Creating a dictionary inside the slides array 
-    slides.append({
-        "id": f"slide{number+1}",
-        "text": indv_slide_txt
-    })
+main(): 
+    #Going through each slide in the presentation
+    for number, page in enumerate(doc):     #to go through in order of the slides
+        raw_text = page.get_text()
+        lines = [line.strip() for line in raw_text.split('\n') if line.strip()]     #creates a list of stripped line that doesn't have any empty lines
+        indv_slide_txt = " ".join(lines)    #puts all the lines from one slide into one paragraph
+        #Creating a dictionary inside the slides array 
+        slides.append({
+            "id": f"slide{number+1}",
+            "text": indv_slide_txt
+        })
 
-with open(output_doc, "w") as file:
-    json.dump(slides, file, indent = 2)
+    #Saves all the information in the ouput_doc
+    with open(output_doc, "w") as file:
+        json.dump(slides, file, indent = 2)
 
-print(f"Successfully Extracted {len(slides)} slides to {output_doc}")
+    print(f"Successfully Extracted {len(slides)} slides to {output_doc}")
+
+main()
